f276d64411de53299537efe9233b725442fb2109
slawoe <info@slawomirernst.de>
2021-03-26 10:11:24 +0100

    outsource the styles in the AboutSection to reuse them in others into styles.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,9 @@
 import home1 from "../assets/img/home1.png";
-import styled from "styled-components";
+import { BasicLayout, Description, Hide, Image } from "../styles";
 
 const AboutSection = () => {
   return (
-    <About>
+    <BasicLayout>
       <Description>
         <div className="title">
           <Hide>
@@ -27,38 +27,8 @@ const AboutSection = () => {
       <Image>
         <img src={home1} alt="guy with a camera" />
       </Image>
-    </About>
+    </BasicLayout>
   );
 };
 
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 export default AboutSection;
