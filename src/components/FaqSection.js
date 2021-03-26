9d7909f338037eaf71f4442b5dbea3952a188237
slawoe <info@slawomirernst.de>
2021-03-26 09:15:03 +0100

    Add FaqSection and import it to AboutUs page

---
+++ src/components/FaqSection.js
@@ -0,0 +1,51 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            aliquam.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            aliquam.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Payment methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            aliquam.
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products Do You Offer</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
+            aliquam.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
