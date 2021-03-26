90d993c3fd5b372f1af00d25d83e0a60339cba62
slawoe <info@slawomirernst.de>
2021-03-26 09:08:33 +0100

    Add ServicesSection and import it to AboutUs page

---
+++ src/components/ServicesSection.js
@@ -0,0 +1,52 @@
+import clock from "../assets/img/clock.svg";
+import diaphragm from "../assets/img/diaphragm.svg";
+import money from "../assets/img/money.svg";
+import teamwork from "../assets/img/teamwork.svg";
+import home2 from "../assets/img/home2.png";
+
+const ServicesSection = () => {
+  return (
+    <div className="services">
+      <div className="description">
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={clock} alt="clock" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={diaphragm} alt="diaphragm" />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={money} alt="money" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={teamwork} alt="teamwork" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div className="image">
+        <img src={home2} alt="home" />
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,11 @@
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
+      <ServicesSection />
     </>
   );
 };
