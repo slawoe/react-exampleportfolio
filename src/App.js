c7945cd67fca6aeac0561214d5c519d49fb77f69
slawoe <info@slawomirernst.de>
2021-07-06 09:34:32 +0200

    Add Router, Switch and Route to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import GlobalStyle from "./GlobalStyles";
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
-      <Nav />
-      <AboutUs />
-    </div>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/work">
+            <OurWork />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+          <Route exact path="/">
+            <AboutUs />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
