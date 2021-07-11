3466943a3ba59ac82f75558acca7748887fd817a
slawoe <info@slawomirernst.de>
2021-07-11 11:03:50 +0200

    Add MovieDetail page

---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { MovieState } from "../movieState";
+import Loading from "../components/Loading";
+import Award from "../components/Award";
+import ImageDisplay from "../components/ImageDisplay";
+
+function MovieDetail() {
+  const history = useHistory();
+  const url = history.location.pathname;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    async function showMovieDetails() {
+      try {
+        const currentMovie = movies.filter(
+          (stateMovie) => stateMovie.url === url
+        );
+        await setMovie(currentMovie[0]);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    showMovieDetails();
+  }, [movies, url]);
+
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      {movie && (
+        <Details>
+          <Headline>
+            <h2>{movie.title}</h2>
+            <img src={movie.mainImg} alt="movie" />
+          </Headline>
+          <Awards>
+            {movie.awards.map((award) => (
+              <Award
+                title={award.title}
+                description={award.description}
+                key={award.title}
+              />
+            ))}
+          </Awards>
+          <ImageDisplay img={movie.secondaryImg} />
+        </Details>
+      )}
+    </>
+  );
+}
+
+const Details = styled.div`
+  color: white;
+`;
+
+const Headline = styled.div`
+  min-height: 90vh;
+  padding-top: 20vh;
+  position: relative;
+  h2 {
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translate(-50%, -10%);
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
+
+const Awards = styled.div`
+  min-height: 80vh;
+  display: flex;
+  margin: 5rem 10rem;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export default MovieDetail;
